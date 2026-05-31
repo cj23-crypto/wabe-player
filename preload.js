0b02e1ac28519a0d0b465667506b6efeb8b18169
cj23-crypto <ep2002497@gmail.com>
2026-05-31 13:47:18 -0600

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,5 +1,5 @@
 const { contextBridge, ipcRenderer } = require("electron");
-
 contextBridge.exposeInMainWorld("electronAPI", {
   openFiles: () => ipcRenderer.invoke("open-files"),
+  openFolder: () => ipcRenderer.invoke("open-folder"),
 });
